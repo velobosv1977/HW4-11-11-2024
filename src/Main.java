8eef12fe601360b89bf5767f0ac8faa4cf91ddf7
velobosv1977 <velobosv33@gmail.com>
2024-11-12 10:17:30 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("ДЗ№3-1. Условный оператор. Урок 2.");
+        System.out.println("Задача 1. Моб.приложение");
+
+    }
+}
